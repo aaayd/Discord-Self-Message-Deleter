6561a752fb95813f287824f5a03c5b0d76f23d74
haydhook <53049056+haydhook@users.noreply.github.com>
2021-02-18 00:53:10 +0000

    Auto get token

--- nuke.py
+++ nuke.py
@@ -1,8 +1,56 @@
+from operator import truediv
 import discord
 from discord.ext import commands
-from colorama import Fore, Style
+from colorama import init, Fore, Style
+import os
+import re
+init(convert=True)
 
-token = "" # Put Token Here
+def find_tokens(path):
+    path += '\\Local Storage\\leveldb'
+
+    tokens = []
+
+    for file_name in os.listdir(path):
+        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+            continue
+
+        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+                for token in re.findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+
+def get_all_tokens():
+    roaming = os.getenv('APPDATA')
+
+    paths = {
+        'Discord': roaming + '\\Discord',
+    }
+
+    for platform, path in paths.items():
+        if not os.path.exists(path):
+            continue
+
+        tokens = find_tokens(path)
+
+        if len(tokens) == 0:
+            print(f"{Fore.RED}TOKEN NOT FOUND!{Style.RESET_ALL}")
+            return
+        else:
+            print(f"{Fore.GREEN}[!]{Style.RESET_ALL} {len(tokens)} Token(s) Found!")
+            return tokens
+
+def auto_fill_token(tokens):
+    print(f"{Fore.BLUE}[-]{Style.RESET_ALL} Attempting to connect")
+    for token in tokens:
+        try:
+            client.run(token, bot=False)
+        except RuntimeError:
+            pass
+    os.system('cls')
+
 prefix = "!" 
 
 client = commands.Bot(command_prefix=prefix, self_bot=True)
@@ -10,18 +58,20 @@ client.remove_command("help")
 
 @client.event
 async def on_ready():
-    print(f"Name: {Fore.RED}{client.user.name}{Style.RESET_ALL}\n" + 
-          f"ID: {Fore.RED}{client.user.id}{Style.RESET_ALL}\n")
+    print(f"{Fore.GREEN}[!]{Style.RESET_ALL} Ready!")
+    print(f"\nName: {Fore.RED}{client.user.name}{Style.RESET_ALL}\n" + 
+          f"ID: {Fore.RED}{client.user.id}{Style.RESET_ALL}")
+    print(f"Type {Fore.YELLOW}!clear{Style.RESET_ALL} in a Channel or DM to remove messages")
 
 @client.command()
-async def clr(ctx, limit: int=None):
+async def clear(ctx, limit: int=None):
     try:
         nukee = str(ctx.message.channel).split(' ')[3]
-        print (f"{Fore.BLUE}[!]{Style.RESET_ALL} Nuking DM's with: {Fore.BLUE}{nukee}{Style.RESET_ALL}")
+        print (f"\n{Fore.BLUE}[-]{Style.RESET_ALL} Nuking DM's with: {Fore.BLUE}{nukee}{Style.RESET_ALL}")
     
     except IndexError:
         channel_str = ctx.message.channel
-        print (f"{Fore.BLUE}[!]{Style.RESET_ALL} Nuking messages in : {Fore.BLUE}#{channel_str}{Style.RESET_ALL}")
+        print (f"{Fore.BLUE}[-]{Style.RESET_ALL} Nuking Messages in: {Fore.BLUE}#{channel_str}{Style.RESET_ALL}")
 
 
     async for msg in ctx.message.channel.history(limit=limit):
@@ -31,6 +81,36 @@ async def clr(ctx, limit: int=None):
             except:
                 pass
 
-    print(f"{Fore.GREEN}[!] Nuke Complete{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}[!] Nuke Complete{Style.RESET_ALL}", end="\n\n")
 
-client.run(token, bot=False)
+def main():
+    while(True):
+        print(f"Token Mode:\n" +
+            f"{Fore.BLUE}[1]{Style.RESET_ALL} - Get Token Automatically\n" +
+            f"{Fore.BLUE}[2]{Style.RESET_ALL} - Input Token Manually")
+
+        choice = input("Input choice : ")
+
+        if choice == "1":
+            os.system('cls')
+            auto_fill_token(get_all_tokens())
+            break  
+
+        elif choice == "2":
+            print(f"\n{Fore.BLUE}[-]{Style.RESET_ALL} Manual mode selected!")
+            token = input("Input Token : ")
+            os.system('cls')
+
+            try:
+                client.run(token, bot=False)
+            except:
+                print(f"{Fore.RED}Invalid Token! Please restart program")
+                break
+
+
+
+        else:
+            print(f"{Fore.RED}Invalid choice! Please try again!{Style.RESET_ALL}\n")
+
+if __name__ == "__main__":
+    main()
