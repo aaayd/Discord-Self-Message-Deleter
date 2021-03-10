493af6e1666e9f8a9ed3c317866e820996576ecb
haydhook <53049056+haydhook@users.noreply.github.com>
2021-03-10 23:46:04 +0000

    Add privately delete chats (via channel id)

--- nuke.py
+++ nuke.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 from colorama import init, Fore, Style
+from time import sleep
+
 import os
 import re
 init(convert=True)
@@ -29,7 +31,7 @@ def get_all_tokens():
     for platform, path in paths.items():
         if not os.path.exists(path):
             continue
-
+ 
         tokens = find_tokens(path)
 
         if len(tokens) == 0:
@@ -56,29 +58,63 @@ client.remove_command("help")
 @client.event
 async def on_ready():
     print(f"{Fore.GREEN}[!]{Style.RESET_ALL} Ready!")
-    print(f"\nName: {Fore.RED}{client.user.name}{Style.RESET_ALL}\n" + 
-          f"ID: {Fore.RED}{client.user.id}{Style.RESET_ALL}")
+    print(f"\nName: {Fore.BLUE}{client.user.name}{Style.RESET_ALL}\n" + 
+          f"ID: {Fore.BLUE}{client.user.id}{Style.RESET_ALL}")
     print(f"Type {Fore.YELLOW}!clear{Style.RESET_ALL} in a Channel or DM to remove messages")
+
+@client.listen()
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        print(f"{Fore.RED}[x] Error{Style.RESET_ALL}: Ensure your limit is a number.", end="\n\n")
+
+@client.command()
+async def clear_id(ctx, id : int=None, skipx : int=0,  limit : int=None):
+    channel = client.get_channel(id)
+    print(f"{Fore.BLUE}[-]{Style.RESET_ALL} Purging {Fore.BLUE}{channel}{Style.RESET_ALL}")
+    count = 0
+    async for msg in channel.history(limit=limit):
+        if client.user.id == msg.author.id:
+            count += 1
+
+    iter = 0
+    async for msg in channel.history(limit=limit):
+        if client.user.id == msg.author.id:
+            iter += 1
+            sleep(0.3)
+            if iter < skipx:
+                print(f"{Fore.BLUE}[{iter}/{count}]{Style.RESET_ALL} {Fore.MAGENTA}SKIPPED{Style.RESET_ALL} message: {Fore.CYAN}{msg.content}{Style.RESET_ALL}")
+                continue
+
+            print (f"{Fore.BLUE}[{iter}/{count}]{Style.RESET_ALL} {Fore.RED}DELETED{Style.RESET_ALL} message: {Fore.CYAN}{msg.content}{Style.RESET_ALL}")
+            await msg.delete()
+
+    print(count)
+
+
 
 @client.command()
 async def clear(ctx, limit: int=None):
+    count = 0
     try:
         nukee = str(ctx.message.channel).split(' ')[3]
-        print (f"\n{Fore.BLUE}[-]{Style.RESET_ALL} Nuking DM's with: {Fore.BLUE}{nukee}{Style.RESET_ALL}")
-    
+        print (f"\n{Fore.BLUE}[-]{Style.RESET_ALL} Purging DM's with: {Fore.BLUE}{nukee}{Style.RESET_ALL}")
+     
     except IndexError:
         channel_str = ctx.message.channel
-        print (f"{Fore.BLUE}[-]{Style.RESET_ALL} Nuking Messages in: {Fore.BLUE}#{channel_str}{Style.RESET_ALL}")
+        print (f"{Fore.BLUE}[-]{Style.RESET_ALL} Purging Messages in: {Fore.BLUE}#{channel_str}{Style.RESET_ALL}")
 
 
     async for msg in ctx.message.channel.history(limit=limit):
+        
         if client.user.id == msg.author.id:
             try:
                 await msg.delete()
+                count += 1
             except:
                 pass
 
-    print(f"{Fore.GREEN}[!] Nuke Complete{Style.RESET_ALL}", end="\n\n")
+    print(f"{Fore.GREEN}[!]{Style.RESET_ALL} Purge Complete!\n" +
+          f"{Fore.BLUE}[-]{Style.RESET_ALL} Removed {Fore.RED}{count}{Style.RESET_ALL} messages.", end="\n\n")
 
 def main():
     while(True):
@@ -97,19 +133,19 @@ def main():
             print(f"\n{Fore.BLUE}[-]{Style.RESET_ALL} Manual mode selected!")
             token = input("Input Token : ")
             
-            if token[0] and token[:-1] == '"':
-                token = token[1:-1]
+            if token[0] and token[-1] == '"':
+                token = str(token[1:-1])
 
             os.system('cls')
-
             try:
                 client.run(token, bot=False)
             except:
-                print(f"{Fore.RED}Invalid Token! Please restart program")
+                print(f"{Fore.RED}Invalid Token! Please restart program{Style.RESET_ALL}")
                 break
 
         else:
             print(f"{Fore.RED}Invalid choice! Please try again!{Style.RESET_ALL}\n")
+
 
 if __name__ == "__main__":
     main()
